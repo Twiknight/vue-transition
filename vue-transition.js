e3a7b2b2c9b31218c6dda64c85a8365a781abaaa
Twiknight <twiknight@163.com>
2015-12-27 11:15:50 +0800

    Add support for amd and npm

--- vue-transition.js
+++ vue-transition.js
@@ -1,4 +1,4 @@
-(function(){
+;(function(){
     var trans = Vue.extend({
         template: '<element></element>',
         props: ['id','options','source'],
@@ -52,5 +52,16 @@
         }
     });
 
-    Vue.component('transition',trans);
+    function install(Vue){
+        Vue.component('transition',trans);
+    }
+
+    if (typeof exports == "object") {
+        module.exports = install
+      } else if (typeof define == "function" && define.amd) {
+        define([], function(){ return install })
+      } else if (window.Vue) {
+        Vue.use(install);
+      }
+
 })();
